ab4e7c350db07610f43db0e623c425d85e75f922
Reu-Amissah <reujoyamissah@gmail.com>
2024-02-14 16:48:44 +0000

    feat: add login & sign up

--- jiraiya-sensei/src/app/app.routes.ts
+++ jiraiya-sensei/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './screens/login/login.component';
+import { SignUpComponent } from './screens/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
   },
 ];
--- jiraiya-sensei/tailwind.config.js
+++ jiraiya-sensei/tailwind.config.js
@@ -5,10 +5,20 @@ module.exports = {
     extend: {
       colors: {
         pureWhite: "#FFFFFF",
-        red: "#FC4747",
+        redy: "#FC4747",
         darkBlue: "#10141E",
         greyishBlue: "#5A698F",
         semiDarkBlue: "#161D2F",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      fontSize: {
+        sm: "13px",
+        base: "15px",
+        md: "18px",
+        lg: "24px",
+        xl: "32px",
       },
     },
   },
