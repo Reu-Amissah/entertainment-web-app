0b5a97dd11a1502164db2d6648f0520a8fd541b2
Reu-Amissah <reujoyamissah@gmail.com>
2024-02-15 09:40:03 +0000

    feat: add tailwind to screens and components

--- jiraiya-sensei/src/app/app.routes.ts
+++ jiraiya-sensei/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './screens/login/login.component';
 import { SignUpComponent } from './screens/sign-up/sign-up.component';
+import { HomeComponent } from './screens/home/home.component';
+import { MoviesComponent } from './screens/movies/movies.component';
+import { SeriesComponent } from './screens/series/series.component';
+import { BookmarksComponent } from './screens/bookmarks/bookmarks.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +15,21 @@ export const routes: Routes = [
   {
     path: 'sign-up',
     component: SignUpComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+  },
+  {
+    path: 'series',
+    component: SeriesComponent,
+  },
+  {
+    path: 'bookmarks',
+    component: BookmarksComponent,
   },
 ];
